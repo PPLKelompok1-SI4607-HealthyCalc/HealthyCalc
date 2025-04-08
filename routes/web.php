03190b9608388a991cd79ee8a6c2e360f8f0d0e8
Dinarkhansas <salsabiladinarkhansa@gmail.com>
2025-04-09 00:13:32 +0700

    dinar

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,7 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-
 
 Route::get('/register', [RegisterController::class, 'show'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
