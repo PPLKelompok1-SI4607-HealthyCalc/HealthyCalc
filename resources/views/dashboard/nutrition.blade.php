55f7392344244e2f2996c482812081c63e18f20a
anestiandinesta <anestiandi05@gmail.com>
2025-04-14 21:08:11 +0700

    Anes - frontend dashboard PBI#7

--- resources/views/dashboard/nutrition.blade.php
+++ resources/views/dashboard/nutrition.blade.php
@@ -9,29 +9,35 @@
         $nutrition = [
             'kalori' => ['consumed' => 1400, 'target' => 2000],
             'protein' => ['consumed' => 60, 'target' => 75],
-            'karbo' => ['consumed' => 180, 'target' => 250],
-            'lemak' => ['consumed' => 50, 'target' => 70],
+            'karbo'   => ['consumed' => 180, 'target' => 250],
+            'lemak'   => ['consumed' => 50, 'target' => 70],
         ];
     @endphp
 
     <div class="row">
         @foreach ($nutrition as $key => $value)
-        <div class="col-md-3 mb-3">
-            <div class="card shadow-sm rounded-4">
-                <div class="card-body text-center">
-                    <h5 class="card-title text-capitalize">{{ $key }}</h5>
-                    <p class="fs-4 mb-1">{{ $value['consumed'] }} / {{ $value['target'] }}</p>
-                    <div class="progress" style="height: 10px;">
-                        <div class="progress-bar bg-success" role="progressbar"
-                            style="width: {{ ($value['consumed'] / $value['target']) * 100 }}%"
-                            aria-valuenow="{{ $value['consumed'] }}"
-                            aria-valuemin="0"
-                            aria-valuemax="{{ $value['target'] }}">
+            @php
+                $percentage = $value['target'] > 0 
+                    ? ($value['consumed'] / $value['target']) * 100 
+                    : 0;
+            @endphp
+
+            <div class="col-md-3 mb-3">
+                <div class="card shadow-sm rounded-4">
+                    <div class="card-body text-center">
+                        <h5 class="card-title text-capitalize">{{ $key }}</h5>
+                        <p class="fs-4 mb-1">{{ $value['consumed'] }} / {{ $value['target'] }}</p>
+                        <div class="progress" style="height: 10px;">
+                            <div class="progress-bar bg-success" role="progressbar"
+                                 style="width: {{ $percentage }}%"
+                                 aria-valuenow="{{ $value['consumed'] }}"
+                                 aria-valuemin="0"
+                                 aria-valuemax="{{ $value['target'] }}">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         @endforeach
     </div>
 </div>
