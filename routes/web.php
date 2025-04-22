931554d0de821b87c3d2d309e45b5af86a35fa84
Stevydea <stevydeapns@gmail.com>
2025-04-22 17:42:12 +0700

    PPLSI4607-6 <update fitur 1 stevy>

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController; // ← ini yang kurang
+use App\Http\Controllers\KaloriNutrisiController;
 
 Route::get('/register', [RegisterController::class, 'show'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 
-use App\Http\Controllers\KaloriNutrisiController;
 
 Route::get('/kalori-nutrisi/create', [KaloriNutrisiController::class, 'create'])->name('kalori-nutrisi.create');
